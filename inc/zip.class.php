c12417dce099ab3040e38002acadd87f797d16c7
朱丰华 <zfh1042@163.com>
2022-06-06 23:30:21 +0800

    Update zip.class.php

--- inc/zip.class.php
+++ inc/zip.class.php
@@ -28,7 +28,7 @@ class ExtendedZip extends ZipArchive {
                 continue;
 
             // zip打包排除项
-            if($filename==".user.ini" || $filename==".git" || $filename==".idea" || $filename==".gitattributes" || $filename==".gitignore"){
+            if($filename==".user.ini" || $filename==".git" || $filename==".idea" || $filename==".gitattributes" || $filename==".gitignore" || $filename=="temp"){
                 continue;
             }
             // Proceed according to type

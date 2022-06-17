c5468447d4a018a650920c6e36ef1aa3f6c7ea8a
朱丰华 <zfh1042@163.com>
2022-06-18 03:48:43 +0800

    目录树检测级别，自动防卡死
    目录树检测级别，自动防卡死

--- admin/tree.php
+++ admin/tree.php
@@ -4,14 +4,19 @@
     force_login();  // 强制登录
 
     $base_dir = force_get_param("base_dir");
+//    die($base_dir);
+//    if($base_dir=="/"){
+//        build_err("不支持根目录");
+//    }
+    // 一次至多查找2000条
+    $limit = 2000;
+
+    // 最大目录层级3层（仅在数据量超过限定数量时，此限制生效，如果数量在可控范围内则无效）
+    $limit_deep = 3;
+
+    $is_beyond_deep = false;
     
-    if($base_dir=="/"){
-        build_err("不支持根目录");
-    }
-    // 一次至多查找1000条
-    $limit = 1000;
-    
-    $encode_dir = re_url_encode($base_dir);
+    $encode_dir = urlencode($base_dir);
     $bp3_tag->assign("base_dir",$base_dir);
     
     $url = "http://pan.baidu.com/rest/2.0/xpan/multimedia?method=listall&path=$encode_dir&access_token=$access_token&order=name&recursion=1&limit=$limit";
@@ -19,9 +24,12 @@
     $result = easy_file_get_content($url);
 
     $arr = m_decode($result);
+
+
     $record = count($arr['list']);
+    $has_more = (bool)$arr['has_more'];
     $bp3_tag->assign("record",$record);  // 取得的记录数
-    $bp3_tag->assign("has_more",$arr['has_more']);  // 取得的记录数
+    $bp3_tag->assign("has_more",$has_more);  // 取得的记录数
 
     $pathStr = explode("/",$base_dir); // 分割根目录
     $base_dir_count = count($pathStr);  // 根目录层级，只记录数量，如 /apps/share，这里取得 2
@@ -35,12 +43,19 @@
 
     $dir_arr[$base_dir] = ['name'=>$lastPath,'deep'=>1,'size'=>0];  // 根目录，深度为 1
 
+    $count_dir = 0;
     foreach ($arr['list'] as $row){
         if($row['isdir']){
-            
+            $count_dir++;
             // 生成子目录info
             $deep = count(explode("/",$row['path']))-$base_dir_count+1;  // 计算子目录层级数，深度动态计算得到
-            
+
+            if($has_more && $deep>$limit_deep){  // 如果还有更多数据，且大于限定层级，说明数据量非常大，丢弃部分数据否则容易造成卡死
+                $is_beyond_deep = true;
+                continue;
+            }
+            if($count_dir)
+
             $dir_info = ['name'=>$row['server_filename'],'deep'=>$deep,'size'=>0,'parent'=>0];
             
             $dir_arr[$row['path']] = $dir_info;
@@ -83,23 +98,23 @@
 
     // 前面得到了层级关系，但是数组本身没有结构关系，现在开始重新排序目录
     $dir_sort = [];
-    
+
     $dir_sort[$base_dir] = $dir_arr[$base_dir];
-    
+
     foreach($dir_arr as $key=>$value)
     {
         //从第2层开始，如果有子目录，并找到其子目录
         $start_loop = 2;
         if($value['deep']==$start_loop){
-            
+
             // 把第2层加入
             $dir_sort[$key] = $dir_arr[$key];
             autoCeil($dir_arr,$start_loop,$key,$dir_sort,$max_dir);
         }
     }
-    // 递归添加层数
-    function autoCeil($dir_arr,$start_loop,$key,$dir_sort,$max_dir){
-        global $dir_sort;
+
+// 递归添加层数
+    function autoCeil(& $dir_arr,$start_loop,$key,& $dir_sort,$max_dir){
         if($start_loop>$max_dir){
             return;
         }
@@ -120,6 +135,10 @@
     foreach ($arr['list'] as $row){
         
         if(!$row['isdir']){
+            $deep = count(explode("/",$row['path']))-$base_dir_count+1;  // 计算子目录层级数，深度动态计算得到
+            if($has_more && $deep>$limit_deep+1){ // 层级太深的，丢弃；
+                continue;
+            }
             // 累计根目录文件夹总大小
             $all_size += $row['size'];
             // 查找文件最后一个 / ，以识别它所在的目录
@@ -152,6 +171,7 @@
     }
     $bp3_tag->assign("all_size",$all_size); // 根文件夹总大小
     $bp3_tag->assign("max_dir",$max_dir);  // 最高层次
+    $bp3_tag->assign("is_beyond_deep",$is_beyond_deep);  // 最高层次
     $bp3_tag->assign("data",$dir_arr);  // 数据
     display();
 
